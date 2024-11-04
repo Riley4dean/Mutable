19334dc74e567e13cf2a3ea1af00001f0395ae0b
deanril000 <rileyd278@gmail.com>
2024-11-04 11:27:55 -0500

    oi oi

---
+++ src/Main.kt
@@ -0,0 +1,26 @@
+fun main() {
+    val CapitalsNearMe = mutableMapOf(
+        "Illinois" to "Chicago",
+        "Kentucky" to "Frankfort",
+        "Ohio" to "Colombus",
+        "Boston" to "Masachusetts"
+    )
+    CapitalsNearMe["Kentucky"] = "FrankFort"
+    CapitalsNearMe["Ohio"] = "Colombus"
+    CapitalsNearMe["Illinois"] = "Chicago"
+    CapitalsNearMe.remove("Boston")
+    println(CapitalsNearMe["Ohio"])
+    println(CapitalsNearMe)
+
+    val HealthyFood = mutableListOf(
+        1, "Apple",
+        2, "Mango"
+
+
+    )
+    HealthyFood.add("Orange")
+    HealthyFood[2] = "Broccoli"
+    HealthyFood.remove("Apple")
+    HealthyFood.removeAt(3)
+    println(HealthyFood)
+}
